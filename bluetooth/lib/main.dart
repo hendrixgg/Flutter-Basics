826ca9de4943469ace4b28ec9d7dd89a0cde2dfc
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-10-29 21:10:07 -0400

    basic bluetooth scan works

--- bluetooth/lib/main.dart
+++ bluetooth/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -7,46 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Bluetooth Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,16 +32,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final flutterReactiveBle = FlutterReactiveBle();
+  var _bleStatus = BleStatus.unknown;
+  var _currentError = const GenericFailure<ScanFailure>(
+      code: ScanFailure.unknown, message: "nothing yet");
+  final List<DiscoveredDevice> _bleDevicesNearby = [];
+  late StreamSubscription<DiscoveredDevice> _scanningSubscription;
+  late StreamSubscription<BleStatus> _statusSubscription;
+  final _scanDuration = const Duration(seconds: 5);
 
-  void _incrementCounter() {
+  bool isScanning = false;
+
+  void _scanForDevices() {
+    // clear the list of devices
+    _bleDevicesNearby.clear();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isScanning = true;
+    });
+    // check the status of the ble module.
+    _statusSubscription = flutterReactiveBle.statusStream.listen((status) {
+      setState(() {
+        _bleStatus = status;
+      });
+    });
+    // switch case pretty much just for comments right now.
+    switch (_bleStatus) {
+      case BleStatus.unknown:
+        break;
+      case BleStatus.unsupported:
+        // should notify the user that ble is unsupported
+        break;
+      case BleStatus.unauthorized:
+        // Should ask for permissions if this happens.
+        break;
+      case BleStatus.poweredOff:
+        // Should notify the user that they need to turn on bluetooth.
+        break;
+      case BleStatus.locationServicesDisabled:
+        break;
+      case BleStatus.ready:
+      // should have no issues scanning here.
+    }
+    _scanningSubscription = flutterReactiveBle.scanForDevices(
+      withServices: [],
+      requireLocationServicesEnabled: false,
+      scanMode: ScanMode.lowLatency,
+    ).listen((DiscoveredDevice device) {
+      setState(() {
+        _bleDevicesNearby.add(device);
+      });
+    }, onError: (error) {
+      setState(() {
+        _currentError = error;
+      });
+    });
+
+    Future.delayed(_scanDuration, () {
+      setState(() {
+        isScanning = false;
+        _statusSubscription.cancel();
+        _scanningSubscription.cancel();
+      });
     });
   }
 
@@ -86,39 +114,39 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: ListView(
+            children: <Widget>[
+              Text('BLE Status: $_bleStatus'),
+              Text('Error: $_currentError'),
+              const Text(
+                'Peripherals Nearby',
+              ),
+              for (DiscoveredDevice peripheral in _bleDevicesNearby)
+                Text(peripheral.name),
+              Text(isScanning
+                  ? 'scanning for devices...'
+                  : _bleDevicesNearby.isEmpty
+                      ? 'no devices found'
+                      : ''),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        // mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _scanForDevices,
+            tooltip: 'Refresh',
+            child: const Icon(Icons.refresh),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
